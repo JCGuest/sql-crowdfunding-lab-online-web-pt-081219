aee67b10413b2694798ddbd784d937b1e3f5cab6
John Guest <jcguest90@gmail.com>
2019-10-20 07:45:15 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY, name Text, age INETEGER);
+
+CREATE TABLE projects (
+    id INTEGER PRIMARY KEY, title TEXT,
+    category TEXT,
+    funding_goal INTEGER, start_date TEXT, 
+    end_date TEXT);
+
+CREATE TABLE pledges (
+    id INTEGER PRIMARY KEY, amount INTEGER,
+    user_id INTEGER, project_id INTEGER);
